6ad772d3477db3f583a28554b3a2f32569a47f75
rotu-me <rugbarotu@gmail.com>
2022-12-16 09:52:36 -0800

    Update printalphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,11 @@
 
 int main(void)
 {
+int i, I;
 for (i='a'; i <= 'z'; i++) 
 putchar(i);
-for (i='A'; i <= 'Z'; i++)
-putchar(i);
+for (I='A'; I <= 'Z'; I++)
+putchar(I);
 putchar('\n');
 return (0);
 }
