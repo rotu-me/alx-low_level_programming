dbbd26c0f86e7c136e83a69647848a02172b47c7
rotu-me <rugbarotu@gmail.com>
2022-12-16 08:59:44 -0800

    Create print-alphbet program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+for (i = 'a'; (i <= 'z'); i++)
+putchar(i)
+putchar('\n');
+return (0);
+}
