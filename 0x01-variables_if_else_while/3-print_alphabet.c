c544af5a245d152254e2d53caacfcaa9a95af463
rotu-me <rugbarotu@gmail.com>
2022-12-16 09:14:50 -0800

    Create print-alphbet program

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: returns 0 if successful
+ */
+
+int main(void)
+{
+char lower_case = [[a..z]];
+char upper_case = [[A..Z]];
+putchar(lower_case);
+putchar(upper_case);
+putchar('\n');
+return (0);
+}
