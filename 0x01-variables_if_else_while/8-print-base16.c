25850cef945c4ade38ee45daa9f38334f4faac5d
rotu-me <rugbarotu@gmail.com>
2022-12-16 11:06:13 -0800

    Create a new program

---
+++ 0x01-variables_if_else_while/8-print-base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+char z;
+for (i = 0; i < 10; i++)
+putchar((i % 10) + '0');
+for (z = 'a'; z <= f; z++)
+putchar(z);
+putchar('\n');
+return (0);
+}
