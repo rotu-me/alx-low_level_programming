dd0603f0e1619018ca0d45c0eb02d327dad973e7
rotu-me <rugbarotu@gmail.com>
2022-12-17 12:00:41 -0800

    Create 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- prints a-z 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+int a = 0;
+while(a < 10)
+{
+for (i = 'a'; i <= 'z'; i++) 
+putchar(i);
+a++;
+}
+}
