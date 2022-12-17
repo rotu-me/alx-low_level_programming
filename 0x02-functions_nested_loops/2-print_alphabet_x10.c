1e9ad2303818f6029e749d7475e1a01588c5c597
rotu-me <rugbarotu@gmail.com>
2022-12-17 12:05:18 -0800

    Update 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,10 @@ void print_alphabet_x10(void)
 int a = 0;
 while(a < 10)
 {
+char i;
 for (i = 'a'; i <= 'z'; i++) 
-putchar(i);
+_putchar(i);
+_putchar('\n');
 a++;
 }
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-putchar.c: prints _putchar
 1-alphabet.c: prints alphabets from a-z
+2-print_alphabet_x10.c: prints alphabet 10 times
