6bcd9561ccf33daaca74197e9936e6b413c431b7
rotu-me <rugbarotu@gmail.com>
2022-12-16 17:49:02 -0800

    Create 102-print_comob5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print a combination of two digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a <= 99; a++)
+{
+for (b = 0; b <= 99; b++)
+{
+if (b > a)
+{
+putchar(a / 10 + '0');
+putchar(a % 10 + '0');
+putchar(' ');
+putchar(b / 10 + '0');
+putchar(b % 10 + '0');
+if( i * 100 + j != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
