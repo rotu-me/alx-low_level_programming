5daf689fac44a47e37c5cd749365f53dce9ba433
rotu-me <rugbarotu@gmail.com>
2022-12-16 16:41:39 -0800

    Create program for advanced task

---
+++ 0x01-variables_if_else_while/100-printcomb3.c
@@ -0,0 +1,27 @@
+#include <stdio>
+
+/**
+ * main - prints a combination of two numbers without repitition
+ * Return: Always ouput 0
+ */
+
+int main(void)
+{
+  int a;
+  int b;
+  for (a = 0; a <= 9; a++)
+    {
+      for (b = a + 1; b <= 9; b++)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+return (0);
+}
