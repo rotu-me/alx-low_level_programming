3573bdbd82be5fac828efec4b0a175061b40b706
rotu-me <rugbarotu@gmail.com>
2022-12-22 15:29:59 -0800

    Update 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,10 @@ char *_strcat(char *dest, char *src)
 int i;
 int j;
 for (i = 0, dest[i] != '\0', i++);
-for (j = 0, src[j] != '\0', j++, i++)
+for (j = 0, src[j] != '\0', j++)
 {
 src[j] = dest[i];
+i++;
 }
 dest[i] = '\0';
 return (dest)
