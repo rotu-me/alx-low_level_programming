84b3954c9cbe35b2343560b0ebc07152f500d18e
rotu-me <roturugba@gmail.com>
2023-01-10 13:11:02 -0800

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all arguments of the program
+ * @ac: argument count
+ * @av: pointer to size ac
+ * Return: NULL if ac == 0 or av == null
+ */
+
+char *argstostr(int ac, char **av)
+{
+int i, j, k, l;
+char *arg;
+size = 0;
+k = 0;
+if (ac == 0 || av == NULL)
+return (NULL);
+i = 0;
+while (i < ac)
+{
+j = 0;
+while (av[i][j])
+{
+size++;
+j++;
+}
+size++;
+j++;
+}
+arg = malloc((sizeof(char) * size) + 1);
+if (arg == NULL)
+return (NULL);
+i = 0;
+while (i < ac)
+{
+j = 0;
+while (av[i][j])
+{
+arg[k] = av[i][j];
+j++;
+k++;
+}
+arg[k] = '\n';
+k++;
+j++;
+}
+arg[k] = '\0';
+return (arg);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,4 @@
 2-str_concat.c: Concatenates two given strings
 3-alloc_grid.c
 4-free_grid.c
+100-argstostr.c
